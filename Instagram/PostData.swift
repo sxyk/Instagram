70223a4977afe2958aa68b1a8e395e3d1f49e4ff
sxyk <yktanaka@sigmaxyz.com>
2017-10-01 21:32:54 +0900

    一通り完成

---
+++ Instagram/PostData.swift
@@ -0,0 +1,58 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by 田中優樹 on 2017/09/29.
+//  Copyright © 2017年 yuki.tanaka. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class PostData: NSObject {
+    var id: String?
+    var image: UIImage?
+    var imageString: String?
+    var name: String?
+    var caption: String?
+    var date: NSDate?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(snapshot: DataSnapshot, myid: String) {
+        self.id = snapshot.key
+        
+        let valueDictionary = snapshot.value as! [String:AnyObject]
+        
+        imageString = valueDictionary["image"] as? String
+        image = UIImage(data: NSData(base64Encoded: imageString!, options: .ignoreUnknownCharacters)! as Data)
+        
+        self.name = valueDictionary["name"] as? String
+        self.caption = valueDictionary["caption"] as? String
+        
+        let time = valueDictionary["time"] as? String
+        self.date = NSDate(timeIntervalSinceReferenceDate: TimeInterval(time!)!)
+        
+        if let likes = valueDictionary["iikes"] as? [String] {
+            self.likes = likes
+        }
+        
+        for liked in self.likes {
+            if liked == myid {
+                self.isLiked = true
+                break
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/PostTableTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  PostTableTableViewCell.swift
+//  Instagram
+//
+//  Created by 田中優樹 on 2017/09/30.
+//  Copyright © 2017年 yuki.tanaka. All rights reserved.
+//
+
+import UIKit
+
+class PostTableTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setPostData(postData: PostData) {
+        self.postImageView.image = postData.image
+        
+        self.captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        let formatter = DateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja-JP") as Locale!
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString = formatter.string(from: postData.date! as Date)
+        self.dateLabel.text = dateString
+        
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: UIControlState.normal)
+        }
+    }
+}
